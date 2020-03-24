8c43913b9f18a3856bbff826aef96587a32f0470
Good Guy <good1.2guy@gmail.com>
2020-03-23 18:39:40 -0600

    make default bit-endian png machine native format

--- cinelerra-5.1/cinelerra/filepng.C
+++ cinelerra-5.1/cinelerra/filepng.C
@@ -229,8 +229,8 @@ int FilePNG::write_frame(VFrame *frame, VFrame *data, FrameWriterUnit *unit)
 				asset->png_use_alpha ?  PNG_COLOR_TYPE_RGB_ALPHA : PNG_COLOR_TYPE_RGB,
 				PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 // does not work (gg 2020/03/17 libpng16 fc31)
-//			if( asset->png_depth == 16 && BC_Resources::little_endian )
-//				png_set_swap(png_ptr);
+			if( asset->png_depth == 16 && BC_Resources::little_endian )
+				png_set_swap(png_ptr);
 			png_write_info(png_ptr, info_ptr);
 			png_write_image(png_ptr, output_frame->get_rows());
 			png_write_end(png_ptr, info_ptr);
